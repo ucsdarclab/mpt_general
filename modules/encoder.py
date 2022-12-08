89d07ffe6a50922e2df853697002d9714c9b44f2
jacobjj <jjj025@ucsd.edu>
2022-12-08 10:08:41 -0800

    PreNorm Encoder layer

--- modules/encoder.py
+++ modules/encoder.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torch
 import torch.utils.checkpoint
 from modules.SubLayers import MultiHeadAttention, PositionwiseFeedForward
+from modules.SubLayers import MultiHeadAttentionPreNorm, PositionwiseFeedForwardPreNorm
 
 from einops.layers.torch import Rearrange
 from einops import rearrange
@@ -51,6 +52,40 @@ class EncoderLayer(nn.Module):
         # enc_output, enc_slf_attn = self.slf_attn(
         #     enc_input, enc_input, enc_input, mask=slf_attn_mask)
 
+        enc_output = self.pos_ffn(enc_output)
+        return enc_output
+
+
+class EncoderLayerPreNorm(nn.Module):
+    ''' Single Encoder layer, that consists of a MHA layers and positiion-wise
+    feedforward layer.
+    '''
+
+    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
+        '''
+        Initialize the module.
+        :param d_model: Dimension of input/output of this layer
+        :param d_inner: Dimension of the hidden layer of hte position-wise feedforward layer
+        :param n_head: Number of self-attention modules
+        :param d_k: Dimension of each Key
+        :param d_v: Dimension of each Value
+        :param dropout: Argument to the dropout layer.
+        '''
+        super(EncoderLayerPreNorm, self).__init__()
+        self.slf_attn = MultiHeadAttentionPreNorm(
+            n_head, d_model, d_k, d_v, dropout=dropout)
+        self.pos_ffn = PositionwiseFeedForwardPreNorm(
+            d_model, d_inner, dropout=dropout)
+
+    def forward(self, enc_input, slf_attn_mask=None):
+        '''
+        The forward module:
+        :param enc_input: The input to the encoder.
+        :param slf_attn_mask: mask for self-attn.
+        '''
+        # Without gradient Checking
+        enc_output = self.slf_attn(
+            enc_input, enc_input, enc_input, mask=slf_attn_mask)
         enc_output = self.pos_ffn(enc_output)
         return enc_output
 
@@ -153,6 +188,72 @@ class Encoder(nn.Module):
 
         for enc_layer in self.layer_stack:
             enc_output = enc_layer(enc_output, slf_attn_mask=None)
+
+        if returns_attns:
+            return enc_output, enc_slf_attn_list
+        return enc_output,
+
+
+class EncoderPreNorm(nn.Module):
+    ''' The encoder of the planner.
+    '''
+
+    def __init__(self, n_layers, n_heads, d_k, d_v, d_model, d_inner, c_space_dim, dropout, n_position):
+        '''
+        Intialize the encoder.
+        :param n_layers: Number of layers of attention and fully connected layer.
+        :param n_heads: Number of self attention modules.
+        :param d_k: Dimension of each Key.
+        :param d_v: Dimension of each Value.
+        :param d_model: Dimension of input/output of encoder layer.
+        :param d_inner: Dimension of the hidden layers of position wise FFN
+        :param c_space_dim: Dimension of the c-space
+        :param dropout: The value to the dropout argument.
+        :param n_position: Total number of patches the model can handle.
+        '''
+        super().__init__()
+
+        # Embedding
+        self.to_embedding = nn.Sequential(
+            nn.Linear(c_space_dim, d_model),
+            nn.ReLU(),
+            nn.Linear(d_model, d_model),
+        )
+        # Position Encoding.
+        # NOTE: Current setup for adding position encoding after patch Embedding.
+        self.position_enc = PositionalEncoding(
+            d_model, n_position=n_position)
+
+        self.dropout = nn.Dropout(p=dropout)
+        self.layer_stack = nn.ModuleList([
+            EncoderLayerPreNorm(d_model, d_inner, n_heads, d_k, d_v, dropout=dropout)
+            for _ in range(n_layers)
+        ])
+
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+
+    def forward(self, input_sequence, returns_attns=False):
+        '''
+        The input of the Encoder should be of dim (b, c, h, w).
+        :param input_sequence: Sequence of the trajectories.
+        :param returns_attns: If True, the model returns slf_attns at each layer
+        '''
+        enc_slf_attn_list = []
+        # Get latent embedding.
+        enc_output = self.to_embedding(input_sequence)
+        enc_output = self.layer_norm(enc_output)
+        
+        # Add position encoding.
+        enc_output = self.position_enc(enc_output)
+
+        enc_output = self.dropout(enc_output)
+        enc_output = self.layer_norm(enc_output)
+
+        for enc_layer in self.layer_stack:
+            enc_output = enc_layer(enc_output, slf_attn_mask=None)
+
+        # Final layer requires a layer-norm
+        enc_output = self.layer_norm(enc_output)
 
         if returns_attns:
             return enc_output, enc_slf_attn_list
