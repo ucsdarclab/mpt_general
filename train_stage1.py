5b2b908ebe73e93897f6936899a71f0586557cad
jacobjj <jjj025@ucsd.edu>
2022-12-08 10:14:24 -0800

    add eps only to very small sigma

--- train_stage1.py
+++ train_stage1.py
@@ -52,7 +52,9 @@ def calculate_reconstruction_loss(input_traj, mu, sigma, mask):
     :returns torch.float:
     '''
     eps = 1e-4
-    dist = MultivariateNormal(mu, torch.diag_embed(sigma+eps))
+    index = sigma<1e-6
+    sigma[index] = sigma[index]+eps
+    dist = MultivariateNormal(mu, torch.diag_embed(sigma))
     return -(dist.log_prob(input_traj)*mask).sum(dim=1).mean()
 
 
