3276a336a4fd41291aa42edad187c72e729f8533
jacobjj <jjj025@ucsd.edu>
2022-12-08 10:19:54 -0800

    shell script to run eval

---
+++ docker/run_eval.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker run -d\
+    --rm \
+    --shm-size="8g"\
+    --gpus all\
+    -v $PWD:/workspace \
+    -v $HOME/mpt_manipulation_data:/root/data \
+    -v $HOME/global_planner_data:/root/data2d \
+    mpt:torch-o3d-pyb-tg \
+    python3 eval_2d.py \
+        --dict_model_folder=/root/data2d/general_mpt/model30/ \
+        --ar_model_folder=/root/data2d/general_mpt/stage2/model7/ \
+        --val_data_folder=/root/data2d/maze4/val \
+        --start=0 \
+        --samples=1000 \
+        --num_paths=1 \
+        --map_type=maze
