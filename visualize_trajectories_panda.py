b9131903d8d579ab59f0e6b21933fa040d18764d
jacobjj <jjj025@ucsd.edu>
2023-01-31 17:16:56 -0800

    updated script to visualize planned paths

--- visualize_trajectories_panda.py
+++ visualize_trajectories_panda.py
@@ -15,17 +15,12 @@ from ompl import base as ob
 from ompl import geometric as og
 
 import time
+import argparse
 
 from panda_utils import q_max, q_min, get_pybullet_server
 from panda_utils import set_simulation_env, set_obstacles, set_robot
+from panda_utils import set_robot_vis, set_position
 
-def set_position(model, joints, jointValue):
-    ''' Set the model robot to the given joint values
-    :param model: pybullet id of link.
-    :param jointValue: joint value to be set
-    '''
-    for jV, j in zip(jointValue, joints):
-        pyb.resetJointState(model, j, jV)
 
 def set_visual_env(client_obj, num_boxes, num_spheres, seed):
     '''
@@ -41,76 +36,81 @@ def set_visual_env(client_obj, num_boxes, num_spheres, seed):
     obstacles = set_obstacles(client_obj, num_boxes=num_boxes, num_spheres=num_spheres, seed = seed)
     return panda, joints, obstacles
 
+
 if __name__ == "__main__":
-    # TODO: Load joint trajectory.
-    env_num = 1133
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--method', help='Train/Predicted', choices=['train', 'predict'])
+    parser.add_argument('--planner', help='Planner', choices=['rrt', 'rrtstar'])
+    parser.add_argument('--env_num', help='int b/w 2000-2500', type=int)
+    args = parser.parse_args()
+
+    env_num = args.env_num
     path_num = 0
 
     p = get_pybullet_server('gui')
 
-    # # =================== Place cupboard =================================
-    # # Spawn the robot.
-    # pandaID, jointsID, _ = set_robot(p)
-    # # Place the desk at a range of -0.8:-0.6 meters
-    # shift = [0.0, -0.6, 0.0]
-    # visualShapeId = p.createVisualShape(shapeType=p.GEOM_MESH,
-    #                                 fileName="assets/cupboard.obj",
-    #                                 rgbaColor=[1, 1, 1, 1],
-    #                                 specularColor=[0.4, .4, 0],
-    #                                 )
-    # collisionShapeId = p.createCollisionShape(shapeType=p.GEOM_MESH,
-    #                                     fileName="assets/cupboard.obj",
-    #                                     )
-    # base_orientation = pyb.getQuaternionFromEuler([np.pi/2, 0.0, 0.0])
-    # obstacle_cupboard = p.createMultiBody(baseMass=0.0,
-    #                 #   baseInertialFramePosition=shift,
-    #                   baseCollisionShapeIndex=collisionShapeId,
-    #                   baseVisualShapeIndex=visualShapeId,
-    #                   basePosition=shift,
-    #                   baseOrientation=base_orientation,
-    #                   useMaximalCoordinates=True
-    #                   )
-
-    # # TODO: Place objects in space.
-
-    # # TODO: Write IK for place location.
-
-    # # TODO: Find path
-    # # ==================================================================
-
-    model_folder = '/root/data/general_mpt/stage2/model6'
-    # planner_type = 'rrtstar'
-    # with open(osp.join(model_folder, f'eval_val_plan_{planner_type}_{2000:06d}.p'), 'rb') as f:
-    #     eval_data = pickle.load(f)
-    # index_num = env_num - 2000
-    # path = eval_data['Path'][index_num]
-    # success = eval_data['Success'][index_num]
-    data_folder = '/root/data/pandav3/train'
-    # path_num = 2
-    # with open(osp.join(data_folder, f'env_{env_num:06d}', f'path_{path_num}.p'), 'rb') as f:
-    #     data = pickle.load(f)
-    # path = data['jointPath']
-    panda, joints, obstacles = set_visual_env(p, 6, 6, seed=env_num)
-    # alpha = np.linspace(0, 1, 10)[:, None]
-    # for i, _ in enumerate(path[:-1]):
-    #     # Interpolate b/w joints.
-    #     tmp_path = (1-alpha)*path[i] + alpha*path[i+1]
-    #     for pos in tmp_path:
-    #         # Set robot position.
-    #         set_position(panda, joints, pos)
-    #         time.sleep(0.1)    
-    # if success:
-    #     # Calculate the FK of the arm.        
-    #     panda, joints, obstacles = set_visual_env(p, 6, 6, seed=env_num)
-    #     alpha = np.linspace(0, 1, 10)[:, None]
-    #     for i, _ in enumerate(path[:-1]):
-    #         # Interpolate b/w joints.
-    #         tmp_path = (1-alpha)*path[i] + alpha*path[i+1]
-    #         for pos in tmp_path:
-    #             # Set robot position.
-    #             set_position(panda, joints, pos)
-    #             time.sleep(0.1)
-    # else:
-    #     print("Couldn't find a successful path")
-
-    # # TODO: Add to the scene.
+    # ==================================================================
+    success = False
+    # ================== Ground Truth Data ===============================
+    if args.method == 'train':
+        dataFolder = "/root/data/panda_models/model0/rrtstar"
+        index_num = env_num - 2000
+        pathFile = osp.join(dataFolder, f"rrtstar_val_plan_{2000+(index_num//100)*100:06d}.p")
+        step_size = 1
+        with open(pathFile, "rb") as f:
+            pathTraj = pickle.load(f)
+        success = pathTraj[index_num%100]["success"]
+        if success:
+            path = pathTraj[index_num%100]['jointPath']
+        else:
+            path = None
+    # ======================================================================
+    # ================== Predicted Data ====================================
+    if args.method == 'predict':
+        model_folder = '/root/data/general_mpt/stage2/model7'
+        planner_type = args.planner
+        step_size = 50
+        with open(osp.join(model_folder, f'eval_val_plan_{planner_type}_simp_{2000:06d}.p'), 'rb') as f:
+            eval_data = pickle.load(f)
+        index_num = env_num - 2000
+        path = eval_data['Path'][index_num]
+        success = eval_data['Success'][index_num]
+    # # ====================================================================
+    # data_folder = '/root/data/pandav3/train'
+    
+    # # Define camera 
+    # viewMatrix = p.computeViewMatrixFromYawPitchRoll(
+    #     cameraTargetPosition=[0, 0, 0],
+    #     distance = 2.2,
+    #     yaw=0,
+    #     pitch=-20.6,
+    #     roll=-2.2,
+    #     upAxisIndex=2
+    # )
+    # projectionMatrix = p.computeProjectionMatrixFOV(
+    #     fov=55.0,
+    #     aspect=1.0,
+    #     nearVal=0.1,
+    #     farVal=3.1
+    # )
+    if success:
+        panda, joints, obstacles = set_visual_env(p, 6, 6, seed=env_num)
+        # panda_start_vis = set_robot_vis(p, path[0], [0, 1, 0, 0.4])
+        # panda_goal_vis = set_robot_vis(p, path[-1], [1, 0, 0, 0.4])
+        alpha = np.linspace(0, 1, step_size)[:, None]
+        time.sleep(2)
+        for i, _ in enumerate(path[:-1]):
+            # Interpolate b/w joints.
+            tmp_path = (1-alpha)*path[i] + alpha*path[i+1]
+            for pos in tmp_path:
+                # Set robot position.
+                set_position(panda, joints, pos)
+                # width, height, rgbImg, depthImg, segImg = p.getCameraImage(
+                #     width=480, 
+                #     height=480,
+                #     viewMatrix=viewMatrix,
+                #     projectionMatrix=projectionMatrix
+                # )
+                time.sleep(0.1)
+    else:
+        print("Couldn't find a successful path")
