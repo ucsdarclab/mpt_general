acc56edbf3409a85519300ed5e708efc29f65de5
jacobjj <jjj025@ucsd.edu>
2023-02-01 08:34:38 -0800

    added decoder model with non-zero cross correlation

--- modules/decoder.py
+++ modules/decoder.py
@@ -1,5 +1,6 @@
 ''' VAE style decoder.
 '''
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -96,3 +97,46 @@ class DecoderPreNorm(nn.Module):
         z_q = self.pos_ffn(z_q)
         var = F.softplus(self.sigma(z_q))
         return (self.mu(z_q)+1)/2, var
+
+
+class DecoderPreNormGeneral(nn.Module):
+    ''' Decoder that takes the latent encoding and generates joint samples.
+    with non-zero cross correlation variables.
+    '''
+
+    def __init__(self, e_dim, h_dim, c_space_dim, dropout=0.5):
+        '''
+        :param e_dim: Dimension of the dictionary vectors.
+        :param h_dim: Dimension of the feedforward networks hidden vector.
+        :param c_space_dim: Dimension of the c-space.
+        :param dropout: Dropout value for the fullyconnected layer.
+        '''
+        super().__init__()
+        self.pos_ffn = PositionwiseFeedForward(e_dim, h_dim, dropout)
+
+        # Layers for returning mean and variance
+        self.mu = nn.Sequential(
+            nn.Linear(e_dim, c_space_dim),
+            nn.Tanh()
+        )
+        self.diag = nn.Sequential(
+            nn.Linear(e_dim, c_space_dim),
+        )
+        self.l = nn.Sequential(
+            nn.Linear(e_dim, int(c_space_dim*(c_space_dim-1)/2))
+        )
+        self.register_buffer('l_index', torch.tril_indices(c_space_dim, c_space_dim, offset=-1))
+        self.c_space_dim = c_space_dim
+
+    def forward(self, z_q):
+        ''' Returns the decoded mean and variance.
+        :param z_q: Latent encoding vectors.
+        :returns tuple: mean and diagonal variance vectors.
+        '''
+        z_q = self.pos_ffn(z_q)
+        D = torch.diag_embed(F.softplus(self.diag(z_q)))
+        L_linear = self.l(z_q)
+        L = torch.diag_embed(torch.ones((z_q.shape[0], z_q.shape[1], self.c_space_dim), device=z_q.device))
+        L[:, :, self.l_index[0], self.l_index[1]] = L_linear
+        covar = torch.matmul(torch.matmul(L, D), L.transpose(2, 3))
+        return (self.mu(z_q)+1)/2, covar
