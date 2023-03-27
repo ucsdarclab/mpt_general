9c513da6ff0dfe9be956f339a706b6fa3f48b492
jacobjj <jjj025@ucsd.edu>
2023-03-27 16:13:51 -0700

    update data collection for shelf

--- docker/run_data_bi_collect.sh
+++ docker/run_data_bi_collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SAMPLES=100
+SAMPLES=25
 for CID in {0..19}
 do
 	docker run -d\
@@ -11,11 +11,10 @@ do
 		-v $HOME/mpt_manipulation_data:/root/data \
 		-v $HOME/global_planner_data:/root/data2d \
 		mpt:torch-o3d-pyb-tg \
-	    python3 dual_arms/collect_data.py \
-			--start=$((1+CID*SAMPLES)) \
-			--samples=$SAMPLES \
-			--num_paths=10 \
-            --log_dir=/root/data/bi_panda/train
+	    python3 dual_arms/dual_arm_shelf.py \
+			--start=$((CID*SAMPLES)) \
+			--num_paths=$SAMPLES \
+            --log_dir=/root/data/bi_panda_shelf/val
 done
 
 # do
