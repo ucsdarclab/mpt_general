2c8dc4a25e03e36111598e8b28a7a2b4efd4c501
jacobjj <jjj025@ucsd.edu>
2023-03-26 22:16:00 -0700

    additional note

--- dual_arms/dual_arm_utils.py
+++ dual_arms/dual_arm_utils.py
@@ -245,6 +245,7 @@ def get_path(start_state, goal_state, si, total_time=10):
     
     return [], [], success
 
+# To collect trajectories for checking self-collision, no obstacles.
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--log_dir', help='Folder where data is saved')
