c9736df3a19a0390427ee1c30b2c28aa22a9554b
Jacob Johnson <jjj025@eng.ucsd.edu>
2023-04-03 17:02:04 -0700

    file to hold fetch related joint limits

---
+++ fetch_utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# 3: -2*np.pi, 2*np.pi
+# 5: -2*np.pi, 2*np.pi
+# 7: -2*np.pi, 2*np.pi
+# Fetch limits:
+# -np.pi , np.pi
+# Index 2, 4, and 5 needs to be bounded
+q_min = np.array([[-1.6056, -1.221,-np.pi, -2.251, -np.pi, -2.16, -np.pi]])
+q_max = np.array([[1.6056, 1.518, np.pi, 2.251, np.pi, 2.16, np.pi]])
+
