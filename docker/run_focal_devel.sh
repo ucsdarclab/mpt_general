461af0c365f99ff83d689e9801c9fbb9003dc098
jacobjj <jjj025@ucsd.edu>
2023-05-15 14:38:46 -0700

    add shell script to run focal image

---
+++ docker/run_focal_devel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+docker run --rm -it \
+		-p $1:8800 \
+		--gpus all \
+		--shm-size="16g"\
+		-e DISPLAY=$DISPLAY \
+		-e QT_X11_NO_MITSHM=1 \
+		-e NVIDIA_DRIVER_CAPABILITIES=all \
+		-v $XAUTH:/root/.Xauthority \
+		-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+		-v $PWD:/workspace \
+		-v $HOME/mpt_manipulation_data:/root/data \
+		-v $HOME/global_planner_data:/root/data2d \
+		mpt:focal-ompl-mod-torch-tg \
+		bash
