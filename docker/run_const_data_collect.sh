5d79a2c38d7a2989a8b3ec0383c0fce36558fcd1
jacobjj <jjj025@ucsd.edu>
2023-05-16 12:51:49 -0700

    update data collection

--- docker/run_const_data_collect.sh
+++ docker/run_const_data_collect.sh
@@ -11,8 +11,9 @@ do
 		-v $HOME/mpt_manipulation_data:/root/data \
 		-v $HOME/global_planner_data:/root/data2d \
 		mpt:focal-ompl-mod-torch-tg \
-	    python3 save_quant_index.py \
+	    python3 panda_constraint_shelf.py \
 			--start=$((CID*SAMPLES)) \
 			--samples=$SAMPLES \
-            --file_dir=/root/data/panda_constraint/val
+            --fileDir=/root/data/panda_constraint/val \
+            --numPaths=1
 done
